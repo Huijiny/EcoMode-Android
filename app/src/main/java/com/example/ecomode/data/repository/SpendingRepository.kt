1832fe7262d098e3c0c83286f75869b579aed05c
Huijiny <jiin20803@gmail.com>
2021-06-09 14:17:59 +0900

    🚀 Add SpendingRepository

---
+++ app/src/main/java/com/example/ecomode/data/repository/SpendingRepository.kt
@@ -0,0 +1,22 @@
+package com.example.ecomode.data.repository
+
+import androidx.annotation.WorkerThread
+import com.example.ecomode.data.room.dao.SpendingDao
+import com.example.ecomode.data.room.entity.SpendingEntity
+
+class SpendingRepository(private val spendingDao: SpendingDao, private val date: String) {
+
+    val spendingsInMonth: List<SpendingEntity> = spendingDao.getSpendingsInMonth(date)
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(spending: SpendingEntity) {
+        spendingDao.insertSpending(spending)
+    }
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun update(spending: SpendingEntity) {
+        spendingDao.updateSpending(spending)
+    }
+
+}
